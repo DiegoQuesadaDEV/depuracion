db103a5122599717467cff460ae00313ff9726ad
DiegoQuesadaDEV <110751513+DiegoQuesadaDEV@users.noreply.github.com>
2022-09-02 05:10:10 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+function fibonacci(num) {
+    const fibonacci = [];
+    fibonacci[0] = 0;
+    fibonacci[1] = 1;
+    for (let i = 2; i < num; i++) {
+    fibonacci[i] = fibonacci[i - 2] + fibonacci[i - 1];
+    }
+    console.log(fibonacci);
+}
+
+
+fibonacci(6)
